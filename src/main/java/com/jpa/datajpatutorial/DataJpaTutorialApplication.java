4f89352995b9b592a0a4f9001f77bc4502ed3e69
oen142 <oen142@naver.com>
2021-01-21 23:14:57 +0900

    Initial commit

---
+++ src/main/java/com/jpa/datajpatutorial/DataJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.datajpatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/datajpatutorial/DataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.datajpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
